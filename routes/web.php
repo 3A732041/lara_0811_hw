484b4b5f7afb50daf3a59a43c5d626381da7f0c5
wang zhen yu <asdfg10510588@gmail.com>
2020-10-29 01:42:38 +0800

    新增三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/about',[AboutController::class,'index']);
+
+Route::get('/news',[NewsController::class,'index']);
+
+
